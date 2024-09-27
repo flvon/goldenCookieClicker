9c4d87ad10d18808397086172157bb27fc9c5502
flvon <35497904+flvon@users.noreply.github.com>
2024-09-27 09:40:34 -0300

    Added dynamic allocation of logs and image files source

--- auto_golden_cookie_clicker.py
+++ auto_golden_cookie_clicker.py
@@ -5,6 +5,7 @@ import pyautogui
 import os
 import logging
 import datetime
+from pathlib import Path
 
 def find_and_click_golden_cookie(img_file_path):
     clicks = 0
@@ -29,14 +30,29 @@ current_dir = os.path.dirname( __file__ )
 
 img_file_name = COOKIE_IMAGE_MONITOR # If you want to use a different image, change this line
 
-img_file_path = os.path.join( current_dir, 'img_searches', img_file_name )
+# Asking for the path for the images to be searched
+user_img_input = input( 'Paste the full folder path where your golden cookie images are. The folder and images must exist prior to the execution.\nWill default to desktop/clicker/searched_images if left empty\n')
+if user_img_input is None or user_img_input == '':
+    img_folder = os.path.join( os.path.join( os.environ['USERPROFILE'] ), 'Desktop', 'clicker', 'searched_images' )
+else:
+    img_folder = user_img_input
+img_file_path = os.path.join( img_folder, img_file_name )
 
 #Setting logging path and file
 now = datetime.datetime.now()
 dt = now.strftime( "%Y%m%d_%H%M" )
-logging_path = os.path.join( current_dir, 'logs', dt + '_golden_cookie_clicker.log' )
+
+# Asking for the path where logs will be saved
+user_logger_input = input( 'Paste the full folder path you want to save your logs. Will default to desktop/clicker/logs if left empty\n')
+if user_logger_input is None or user_logger_input == '':
+    logger_folder = os.path.join( os.path.join( os.environ['USERPROFILE'] ), 'Desktop', 'clicker', 'logs' )
+else:
+    logger_folder = user_logger_input
+
+logging_path = os.path.join( logger_folder, dt + '_golden_cookie_clicker.log' )
 
 # Creating logging file if it doesn't exist
+Path( logger_folder ).mkdir( parents=True, exist_ok=True )
 file = open( logging_path, 'a' )
 file.close()
 
